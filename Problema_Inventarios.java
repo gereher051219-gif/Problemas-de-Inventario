3563ab91cf610912217886f1aa162a5ba37fecf4
gereher051219-git <gereher121905@gmail.com>
2025-11-08 21:42:55 -0600

    avanze del programa parte 1

--- Problema_Inventarios.java
+++ Problema_Inventarios.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Problema_Inventarios {
 
     public static void main(String[] args) {
-        
+        Scanner ingresarDatos = new Scanner(System.in);
+        //pedir al usuario que ingrese los daots correspondientes
+        System.out.println("Ingrese la demandaa diaria: ");
+        double demandaD = ingresarDatos.nextDouble();
+        System.out.println("Ingrese el costo de pedido: ");
+        double k = ingresarDatos.nextDouble();
+        System.out.println("Ingrese el costo de almacenamiento: ");
+        double h = ingresarDatos.nextDouble();
+        System.out.println("Ingrese el tiempo de entrega del pedido: ");
+        double L = ingresarDatos.nextDouble();
+
+        //solucion de y*
+        double y;
+        y = Math.sqrt((2*k*demandaD/h));
+        System.out.println("La cantidad de pedido es: " + y);
+
     }
 }
