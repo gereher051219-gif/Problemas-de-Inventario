45b7061b6c39171c20db21bf975bc587b4d1e6bf
gereher051219-git <gereher121905@gmail.com>
2025-11-09 23:21:02 -0600

    Avanze del programa parte 2

--- Problema_Inventarios.java
+++ Problema_Inventarios.java
@@ -6,18 +6,28 @@ public class Problema_Inventarios {
         Scanner ingresarDatos = new Scanner(System.in);
         //pedir al usuario que ingrese los daots correspondientes
         System.out.println("Ingrese la demandaa diaria: ");
-        double demandaD = ingresarDatos.nextDouble();
+        float demandaD = ingresarDatos.nextFloat();
         System.out.println("Ingrese el costo de pedido: ");
-        double k = ingresarDatos.nextDouble();
+        float k = ingresarDatos.nextFloat();
         System.out.println("Ingrese el costo de almacenamiento: ");
-        double h = ingresarDatos.nextDouble();
+        float h = ingresarDatos.nextFloat();
         System.out.println("Ingrese el tiempo de entrega del pedido: ");
-        double L = ingresarDatos.nextDouble();
+        float L = ingresarDatos.nextFloat();
 
         //solucion de y*
         double y;
         y = Math.sqrt((2*k*demandaD/h));
         System.out.println("La cantidad de pedido es: " + y);
+        //solucion de t0
+        float t0, n, Le;
+        t0 = (float) (y / demandaD);
+        //L/t0
+        n = (float)Math.floor(L/t0);          
+        //Le              
+        Le = L - (n * t0);
+        System.out.println("Le es: " + Le); 
+        Le = Le * demandaD;
+        System.out.println("El punto de reorden: " + Le);
 
     }
 }
