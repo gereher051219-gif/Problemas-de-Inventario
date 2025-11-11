f1583915325c1ed493b6ccf6c63cf7b29ea9de4d
gereher051219-git <gereher121905@gmail.com>
2025-11-11 13:47:54 -0600

    Avance terminado

--- Problema_Inventarios.java
+++ Problema_Inventarios.java
@@ -6,28 +6,33 @@ public class Problema_Inventarios {
         Scanner ingresarDatos = new Scanner(System.in);
         //pedir al usuario que ingrese los daots correspondientes
         System.out.println("Ingrese la demandaa diaria: ");
-        float demandaD = ingresarDatos.nextFloat();
+        double demandaD = ingresarDatos.nextDouble();
         System.out.println("Ingrese el costo de pedido: ");
-        float k = ingresarDatos.nextFloat();
+        double k = ingresarDatos.nextDouble();
         System.out.println("Ingrese el costo de almacenamiento: ");
-        float h = ingresarDatos.nextFloat();
+        double h = ingresarDatos.nextDouble();
         System.out.println("Ingrese el tiempo de entrega del pedido: ");
-        float L = ingresarDatos.nextFloat();
+        double L = ingresarDatos.nextDouble();
 
         //solucion de y*
         double y;
         y = Math.sqrt((2*k*demandaD/h));
         System.out.println("La cantidad de pedido es: " + y);
         //solucion de t0
-        float t0, n, Le;
-        t0 = (float) (y / demandaD);
+        double t0, n, Le;
+        t0 = Math.floor((y / demandaD)*100)/100;
+        System.out.println("(t0): Pedir cada: " + t0 + " dias");
         //L/t0
-        n = (float)Math.floor(L/t0);          
+        n = Math.floor(L/t0);          
         //Le              
         Le = L - (n * t0);
-        System.out.println("Le es: " + Le); 
-        Le = Le * demandaD;
-        System.out.println("El punto de reorden: " + Le);
+        double puntoR = Le * demandaD;
+        System.out.println("Reordenar cuando el inventario llegue a: " + puntoR);
+
+        //tcu Costo toatl de inventario
+        double tcu;
+        tcu = k / (y/demandaD) + h * y/2;
+        System.out.println("El costo total de inventario es de: " + tcu + " pesos" );
 
     }
 }
